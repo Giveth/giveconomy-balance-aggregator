85bb0a552133c639bb49db9c8c234f3c716ba518
Amin Latifi <a.latifi.al@gmail.com>
2023-05-10 16:26:56 +0330

    Created balance get service

--- src/modules/balance/balance.service.ts
+++ src/modules/balance/balance.service.ts
@@ -18,12 +18,48 @@ export class BalanceService {
     return this.balanceRepository.save(balance);
   }
 
-  async getBalance(address: string, network: number): Promise<Balance> {
-    return this.balanceRepository.findOneOrFail({
-      where: {
+  /**
+   *
+   * @param params {
+   *   address: string;
+   *   network: number;
+   *   timestamp?: number; // seconds since epoch
+   *   block?: number; // block number
+   * }
+   */
+  async getBalance(params: {
+    address: string;
+    network: number;
+    timestamp?: number;
+    block?: number;
+  }): Promise<Balance> {
+    const { address, network, timestamp, block } = params;
+    let query = this.balanceRepository
+      .createQueryBuilder('balance')
+      .where('balance.address = :address and balance.network = :network', {
         address,
         network,
-      },
-    });
+      });
+    // add timestamp query if exists
+
+    switch (true) {
+      case timestamp !== undefined:
+        query = query.andWhere(
+          `balance.timeRange @> to_timestamp(:timestamp)`,
+          {
+            timestamp: Math.floor(timestamp),
+          },
+        );
+        break;
+
+      case block !== undefined:
+        query = query.andWhere(`balance.blockRange @> ${Math.floor(block)}`);
+        break;
+
+      default:
+        query = query.andWhere('upper_inf(balance.blockRange)');
+    }
+
+    return query.getOne();
   }
 }
--- test/modules/balance/balance.service.spec.ts
+++ test/modules/balance/balance.service.spec.ts
@@ -43,4 +43,80 @@ describe('BalanceService', () => {
       expect(balance.balance).toBe('1000000000000000000');
     });
   });
+
+  describe('get balance', () => {
+    const baseUserData = {
+      address: '0x123456789',
+      network: 1,
+    };
+
+    beforeEach(async () => {
+      await service.balanceRepository.clear();
+      // Fill sample data
+      await service.create({
+        ...baseUserData,
+        balance: '1000000000000000000',
+        timeRange: '[2021-01-01,2021-01-02)',
+        blockRange: '[1000,2000)',
+      });
+      await service.create({
+        ...baseUserData,
+        balance: '2000000000000000000',
+        timeRange: '[2021-01-02,2021-01-03)',
+        blockRange: '[2000,3000)',
+      });
+      await service.create({
+        ...baseUserData,
+        balance: '3000000000000000000',
+        timeRange: '[2021-01-03,)',
+        blockRange: '[3000,)',
+      });
+    });
+
+    it('get simple balance', async () => {
+      await service.balanceRepository.clear();
+      const data = {
+        ...baseUserData,
+        balance: '1000000000000000000',
+        timeRange: '[2021-01-01,2021-01-02)',
+        blockRange: '[1,)',
+      };
+      await service.create(data);
+      const balance = await service.getBalance({
+        address: data.address,
+        network: data.network,
+      });
+      expect(balance).not.toBeNull();
+      expect(balance).toBeTruthy();
+      expect(balance.address).toBe(data.address);
+      expect(balance.network).toBe(data.network);
+    });
+
+    it('get balance by timestamp and block', async () => {
+      let balance = await service.getBalance({
+        address: baseUserData.address,
+        network: baseUserData.network,
+        timestamp: new Date('2021-01-02 GMT').getTime() / 1000,
+      });
+      expect(balance).toBeTruthy();
+      expect(balance.balance).toBe('2000000000000000000');
+
+      balance = await service.getBalance({
+        address: baseUserData.address,
+        network: baseUserData.network,
+        block: 1533,
+      });
+      expect(balance).toBeTruthy();
+      expect(balance.balance).toBe('1000000000000000000');
+    });
+
+    it('get latest balance when no timestamp or block is provided', async () => {
+      const balance = await service.getBalance({
+        address: baseUserData.address,
+        network: baseUserData.network,
+      });
+      expect(balance).toBeTruthy();
+      expect(balance.balance).toBe('3000000000000000000');
+    });
+  });
 });
