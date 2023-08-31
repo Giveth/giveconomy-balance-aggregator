3adb56f9e4f2b5fa093b802f2ec441bc5238c3e2
Amin Latifi <a.latifi.al@gmail.com>
2023-08-31 11:25:10 +0330

    Fixed env var name

--- src/modules/configure.module.ts
+++ src/modules/configure.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 const configureModule = ConfigModule.forRoot({
   envFilePath:
     'config/' +
-    (process.env.node_env === 'production'
+    (process.env.NODE_ENV === 'production'
       ? 'production.env'
       : 'development.env'),
   isGlobal: true,
