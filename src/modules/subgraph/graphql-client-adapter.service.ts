ac01bb374e75e8f0db50e99d369c94bfbddf2906
Amin Latifi <a.latifi.al@gmail.com>
2024-06-10 19:36:19 +0330

    Added subgraph domain field support

--- src/modules/subgraph/graphql-client-adapter.service.ts
+++ src/modules/subgraph/graphql-client-adapter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 
 export type SubgraphBalanceChangeEntity = {
@@ -12,6 +13,11 @@ export type SubgraphBalanceChangeEntity = {
 };
 @Injectable()
 export class GraphqlClientAdapterService {
+  private origin;
+  constructor(readonly configService: ConfigService) {
+    this.origin =
+      this.configService.get<string>('SUBGRAPH_DOMAIN') || 'https://giveth.io';
+  }
   async getBalanceChanges(params: {
     subgraphUrl: string;
     contractAddress: string;
@@ -56,7 +62,15 @@ export class GraphqlClientAdapterService {
       }
       `;
 
-    const result = await axios.post(subgraphUrl, { query });
+    const result = await axios.post(
+      subgraphUrl,
+      { query },
+      {
+        headers: {
+          Origin: this.origin,
+        },
+      },
+    );
     const { balanceChanges, _meta } = result.data.data;
     return {
       balanceChanges,
